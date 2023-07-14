be8ff02f01d6793f3739e9a6607c2dc59551fcdc
JiangTaoHou <925147954@qq.com>
2023-07-15 00:17:53 +0800

    1.最后一次重构，抽取方法

--- Main.go
+++ Main.go
@@ -2,18 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
-		return "Hola, " + name
-	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
 func main() {
 	fmt.Println(Hello("Chris", "Spanish"))
 }
