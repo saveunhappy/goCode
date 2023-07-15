4bd41f4b06afa56b4e0376a5efc6823de9e41491
JiangTaoHou <925147954@qq.com>
2023-07-15 18:04:51 +0800

    最常用的应该是这种吧，不需要指定数组的长度

--- arraysLearn/array_test.go
+++ arraysLearn/array_test.go
@@ -2,16 +2,42 @@ package main
 
 import "testing"
 
+//func TestSum(t *testing.T) {
+//
+//	numbers := [5]int{1, 2, 3, 4, 5}
+//
+//	got := Sum(numbers)
+//	want := 15
+//
+//	if got != want {
+//		t.Errorf("got %d want %d given, %v", got, want, numbers)
+//	}
+//}
+
 func TestSum(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+		got := Sum(numbers)
+		want := 15
 
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
-	}
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
 }
 
 //func Sum(numbers [5]int) int {
@@ -21,7 +47,7 @@ func TestSum(t *testing.T) {
 //	}
 //	return sum
 //}
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
