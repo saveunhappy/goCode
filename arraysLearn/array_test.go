33dd6006d8f9724602db77f3a42368760a234cc8
JiangTaoHou <925147954@qq.com>
2023-07-15 18:02:16 +0800

    感觉没有for循环好用，不如Java的迭代器

--- arraysLearn/array_test.go
+++ arraysLearn/array_test.go
@@ -14,10 +14,17 @@ func TestSum(t *testing.T) {
 	}
 }
 
+//func Sum(numbers [5]int) int {
+//	sum := 0
+//	for i := 0; i < 5; i++ {
+//		sum += numbers[i]
+//	}
+//	return sum
+//}
 func Sum(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
 }
