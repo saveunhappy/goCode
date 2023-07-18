4762787ebfb9b52043c56084efff402f158c8bc4
JiangTaoHou <925147954@qq.com>
2023-07-18 21:08:12 +0800

    1.编写正确的代码

--- mapTest/dictionary.go
+++ mapTest/dictionary.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
@@ -7,5 +9,10 @@ func Search(dictionary map[string]string, word string) string {
 type Dictionary map[string]string
 
 func (d Dictionary) Search(word string) (string, error) {
-	return d[word], nil
+	definition, ok := d[word]
+	if !ok {
+		return "", errors.New("could not find the word you were looking for")
+	}
+
+	return definition, nil
 }
