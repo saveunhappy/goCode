cf27b40ad613934cd03f481e77d276b6864dfc65
JiangTaoHou <925147954@qq.com>
2023-07-18 21:09:25 +0800

    1.提取出错误

--- mapTest/dictionary.go
+++ mapTest/dictionary.go
@@ -8,10 +8,12 @@ func Search(dictionary map[string]string, word string) string {
 
 type Dictionary map[string]string
 
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 	if !ok {
-		return "", errors.New("could not find the word you were looking for")
+		return "", ErrNotFound
 	}
 
 	return definition, nil
