b79a40a44a6d3e2c3e16df5b008c1f950549b8eb
JiangTaoHou <925147954@qq.com>
2023-07-18 22:16:06 +0800

    1.健壮性代码

--- mapTest/dictionary.go
+++ mapTest/dictionary.go
@@ -1,10 +1,5 @@
 package main
 
-const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
-)
-
 type DictionaryErr string
 
 func (e DictionaryErr) Error() string {
@@ -39,6 +34,23 @@ func (d Dictionary) Search(word string) (string, error) {
 	return definition, nil
 }
 
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
 }
--- mapTest/dictionary_test.go
+++ mapTest/dictionary_test.go
@@ -33,25 +33,26 @@ func assertStrings(t testing.TB, got, want string) {
 	}
 }
 func TestAdd(t *testing.T) {
-	t.Run("new word", func(t *testing.T) {
-		dictionary := Dictionary{}
-		word := "test"
-		definition := "this is just a test"
-
-		err := dictionary.Add(word, definition)
-
-		assertError(t, err, nil)
-		assertDefinition(t, dictionary, word, definition)
-	})
-
 	t.Run("existing word", func(t *testing.T) {
 		word := "test"
 		definition := "this is just a test"
 		dictionary := Dictionary{word: definition}
-		err := dictionary.Add(word, "new test")
+		newDefinition := "new definition"
 
-		assertError(t, err, ErrWordExists)
-		assertDefinition(t, dictionary, word, definition)
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
 
