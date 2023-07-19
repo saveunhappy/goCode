70bb470b865f682d6cd01a9395130ff0ee46d3ec
JiangTaoHou <925147954@qq.com>
2023-07-19 20:25:37 +0800

    1.写足够的代码令测试通过

--- mocking/mockingMain.go
+++ mocking/mockingMain.go
@@ -3,8 +3,15 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"os"
 )
 
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, "Go!")
+}
+func main() {
+	Countdown(os.Stdout)
 }
